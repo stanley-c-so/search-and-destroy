d321952eff1ab76c67174d3dc879a90324a105e6
stanley-c-so <stanley.c.so@gmail.com>
2019-05-24 11:50:55 -0400

    finished binarySearch

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,43 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+
+	// RECURSIVE SOLUTION:
+
+	// const midpoint = Math.floor(array.length / 2);
+	// if (array[midpoint] === target) {
+	// 	return true;
+	// } else if (array.length) {
+	// 	if (target < array[midpoint]) {
+	// 		return binarySearch(array.slice(0, midpoint), target);
+	// 	} else {
+	// 		return binarySearch(array.slice(midpoint + 1), target);
+	// 	}
+	// } else {
+	// 	return false;
+	// }
+
+	// EXTRA CREDIT:
+
+	let left = 0;
+	let right = array.length - 1;
+	let midpoint = Math.floor((right - left) / 2);
+
+	while (left !== right) {
+		if (array[midpoint] === target) {
+			return true;
+		} else if (target < array[midpoint]) {
+			right = midpoint - 1;
+		} else {
+			left = midpoint + 1;
+		}
+		midpoint = Math.floor((right - left) / 2);
+	}
+	if (array[midpoint] === target) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 /*
